f76bbc6c440daf57ffa95ee1b29d92952d65e77e
Josh D <joshdorland@icloud.com>
2022-05-06 20:53:40 -0400

    Update

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ let header = document.querySelector("header");
 window.addEventListener("scroll", () => {
     header.classList.toggle("header-active", window.scrollY > 0);
 });
+
+jQuery(document).scroll(function(){
+    if(jQuery(this).scrollTop() > 300)
+    {   
+       jQuery('#navigation').css({"background":"red"});
+    } else {
+       jQuery('#navigation').css({"background":"transparent"});
+    }
+});
